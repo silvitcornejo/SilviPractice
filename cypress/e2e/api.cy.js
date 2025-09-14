77f1c5da887836cad338ced25c2c66e3892bb7f0
Silvit <silvi.cornejoo@gmail.com>
2025-09-14 14:15:57 +0200

    api

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,28 +1,24 @@
 describe('API Tests', () => {
-  it('GET /api/productsList - should return a list of products', () => {
-    cy.request('GET', 'https://www.automationexercise.com/api/productsList')
-      .then((response) => {
-        expect(response.status).to.eq(200)
+    let productsBody
 
-        // convertr body to JSON if it's a string
-        const body = typeof response.body === 'string' ? JSON.parse(response.body) : response.body
+    before (() => {
+        cy.request('GET', 'https://www.automationexercise.com/api/productsList')
+        .then((response) => {
+            expect(response.status).to.eq(200)
 
-        expect(body).to.have.property('products')
-        expect(body.products).to.be.an('array') // assert that products is an array
-      })
-  })
-})
-
-describe('API Tests', () => {
-  it('GET /api/productsList - should return a list of products', () => {
-    cy.request('GET', 'https://www.automationexercise.com/api/productsList')
-      .then((response) => {
-        expect(response.status).to.eq(200)
-
-        // convertr body to JSON if it's a string
-        const body = typeof response.body === 'string' ? JSON.parse(response.body) : response.body  
-        expect(body.products).to.have.length(34) // assert that there are 34 products
-         })
-          })
-           })
+            // convertr body to JSON if it's a string
+            productsBody = typeof response.body === 'string' ? JSON.parse(response.body) : response.body
+            }) 
+            }) 
         
+
+  it('GET /api/productsList - should return status 200 and products array', () => {       
+        expect(productsBody).to.have.property('products')
+        expect(productsBody.products).to.be.an('array') // assert that products is an array
+      })
+
+
+  it('GET /api/productsList - should return a list of 34 products', () => {
+        expect(productsBody.products).to.have.length(34) // assert that there are 34 products})
+    })
+  })         
