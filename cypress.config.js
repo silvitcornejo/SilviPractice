1204bd6c0fdc81a0ddeb5037e3665f6c7b065f52
Silvit <silvi.cornejoo@gmail.com>
2025-09-14 17:01:39 +0200

    add more coverage

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,13 @@ module.exports = defineConfig({
      // --- Setup screenshots ---
     screenshotOnRunFailure: true, // take a screenshot when a test fails
     screenshotsFolder: 'cypress/screenshots', // folder where screenshots are saved
+
+     // --- Retries globales ---
+    retries: {
+      runMode: 2,   // reintenta 2 veces cuando corres `cypress run`
+      openMode: 1   // reintenta 1 vez cuando corres `cypress open`
+    },
+    
     setupNodeEvents(on, config) {
     },
   },
--- cypress/support/pageobjectmodel/homepage.js
+++ cypress/support/pageobjectmodel/homepage.js
@@ -9,3 +9,58 @@ Cypress.Commands.add('CarrouselFunctionality', (selector) => {
     cy.get('.item.active').should('have.length', 1)
   })
 })
+
+
+Cypress.Commands.add('AddProductsToCart', () => {
+  cy.get('.product-image-wrapper').then($products => {
+    // total amount of productsS
+    const productCount = $products.length;
+
+    // obtaing two random indexes to add to cartS
+    const randomIndexes = [];
+    while (randomIndexes.length < 2) {
+      const randomIndex = Math.floor(Math.random() * productCount);
+      if (!randomIndexes.includes(randomIndex)) {
+        randomIndexes.push(randomIndex);
+      }
+    }
+
+    // add first random product
+    cy.wrap($products)
+      .eq(randomIndexes[0])
+      .trigger('mouseover')
+      .find('.add-to-cart')
+      .first()
+      .click();
+
+    cy.get('.modal-content').should('be.visible').within(() => {
+      cy.get('.btn').contains('Continue Shopping').click();
+    });
+
+    // add second random product
+    cy.wrap($products)
+      .eq(randomIndexes[1])
+      .trigger('mouseover')
+      .find('.add-to-cart')
+      .first()
+      .click();
+
+    cy.get('.modal-content').should('be.visible').within(() => {
+      cy.get('p a').contains('View Cart').click();
+    });
+  });
+
+  // check cart has 2 products
+  cy.url().should('include', '/view_cart');
+  cy.get('tbody tr').should('have.length', 2).each($product => {
+    cy.wrap($product).find('.cart_price').should('be.visible');
+    cy.wrap($product).find('.cart_quantity').should('be.visible');
+    cy.wrap($product).find('.cart_total').should('be.visible');
+  });
+});
+
+Cypress.Commands.add('RemoveProductsFromCart', () => {
+  cy.get('.cart_quantity_delete').click({ multiple: true })
+  cy.get('tbody tr').should('have.length', 0)
+    cy.get('#empty_cart').should('contain.text', 'Cart is empty! Click here to buy products.')
+})
